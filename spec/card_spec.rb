19570b692aa3f0753decb014c8d6c86c4a28814f
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-16 16:27:02 -0700

    clean up card spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,11 +5,13 @@ require "minitest/pride"
 require "card"
 
 describe Card do
+
   card_suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
   card_values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack",
       "Queen", "King", "Ace"]
 
   it "has a suit" do
+
     card_value = card_values[0]
 
     card_suits.each do |card_suit|
@@ -20,6 +22,7 @@ describe Card do
   end
 
   it "has a value" do
+
     card_suit = card_suits[0]
 
     card_values.each do |card_value|
